d9b8019920d843bc97841f0598d96a8d15488f5f
atifsayed22 <sayedatif4321@gmail.com>
2026-02-04 14:50:13 +0530

    add new vercel url to cors

--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ app.use(cors({
     origin: [
       "http://localhost:5173",
       "https://net-sphere-sfar.vercel.app",
+      "https://net-sphere-xfwp.vercel.app",
       "https://net-sphere-xfwp-git-main-sayedatif4321-1267s-projects.vercel.app"
     ],
     credentials: true
